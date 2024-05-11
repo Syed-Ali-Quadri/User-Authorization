b41b5fb25b9d619e6a7eafed6850db22843813a5
Syed Ali Quadri <syedaliquadri100@gmail.com>
2024-05-12 03:54:35 +0530

    Setup V1.2 - Changes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// Import
 const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
 const userModel = require("./model/usermodel");
 
+// Setting up ExpressJS
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,11 +19,13 @@ app.get("/", (req, res) =>{
 app.get("/register", (req, res) =>{
     res.render("register");
 });
+
 // Profile Route
 app.get("/profile", (req, res) =>{
     res.render("profile");
 });
 
+// Running Port
 app.listen(port, ()=>{
     console.log(`Listening 127.0.0.1:${port}`);
 }); 
