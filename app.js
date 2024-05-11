f1e914bb5a036c602763e846a373f9d5f426eec5
Syed Ali Quadri <syedaliquadri100@gmail.com>
2024-05-12 03:26:48 +0530

    Backend/Database-MongoDB - Done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
+const userModel = require("./model/usermodel");
 
 app.set("view engine", "ejs");
 app.use(express.json());
